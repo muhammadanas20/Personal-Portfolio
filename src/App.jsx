1080a9db1ebe5b2a11d4b8c1cbfc4583317702d4
muhammadanas20 <muhammadanasashfaq2006@gmail.com>
2026-01-20 20:05:19 +0500

    clickspark  animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
 // src/App.jsx
-import { Navbar } from "@/lay/navbar";       
-import { Hero } from "@/sections/hero";      
-import { About } from "@/sections/about";    
-import { Projects } from "@/sections/projects"; 
-import { Education } from "@/sections/education"; 
-import { Contact } from "@/sections/contact"; 
+import { Navbar } from "@/lay/navbar";
+import { Hero } from "@/sections/hero";
+import { About } from "@/sections/about";
+import { Projects } from "@/sections/projects";
+import { Education } from "@/sections/education";
+import { Contact } from "@/sections/contact";
 import { Footer } from "@/lay/footer";
+import ClickSpark from "@/components/clickspark";
+
 const App = () => {
   return (
     <div className="min-h-screen overflow-x-hidden">
-      <Navbar />
-      <main>
-        <Hero />
-        <About />
-        <Projects />
-        <Education />
-        <Contact />
-      </main>
-       <Footer />
+      <ClickSpark
+        sparkColor="primary"
+        sparkSize={10}
+        sparkRadius={15}
+        sparkCount={8}
+        duration={500}
+      >
+        {" "}
+        <Navbar />
+        <main>
+          <Hero />
+          <About />
+          <Projects />
+          <Education />
+          <Contact />
+        </main>
+        <Footer />
+      </ClickSpark>
     </div>
   );
 };
