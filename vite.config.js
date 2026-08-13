a57221adfcc4263254ac3f037cb2fb13bd0fc6be
Muhammad Anas <k250899@nu.edu.pk>
2026-08-13 21:48:44 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
-import { fileURLToPath } from "url"; // Import this
-
-// Fix for __dirname in modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { defineConfig } from 'vite'
+import react from '@vitejs.plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://anastrix.live',
+      // Add client-side paths if you have multiple pages:
+      dynamicRoutes: ['/about', '/projects', '/contact'],
+      robots: [{ userAgent: '*', allow: '/' }]
+    })
+  ]
+})
